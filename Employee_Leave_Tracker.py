21f8cae57b95c87aa4a266d4a2f74c4754cb54c1
aktiv-abhay-sakariya <abhay.sakariya@aktivsoftware.com>
2026-05-27 12:32:56 +0530

    2 commit

--- Employee_Leave_Tracker.py
+++ Employee_Leave_Tracker.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-
+from datetime import date, datetime
 
 def analyzes_employee_leave_generates_categorized(leaves, current_date):
     """
@@ -92,4 +92,5 @@ while True:
             break
     except Exception:
         print('Enter only number')
+print(analyzes_employee_leave_generates_categorized(leaves, current_date))
 print(analyzes_employee_leave_generates_categorized(leaves, current_date))
